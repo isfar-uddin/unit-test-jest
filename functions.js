68eadd2418fe388451bfa42fb3aeff6ba6fa32fc
isfar-uddin <isfarruett@gmail.com>
2019-03-01 20:51:41 +0600

    Write a test for createUser function

--- functions.js
+++ functions.js
@@ -1,3 +1,9 @@
 module.exports = {
     add: (a,b) => a+b,
+    createUser: () => {
+        const firstName = "Isfar";
+        const lastName = "Uddin";
+        const user = {firstName,lastName}
+        return user;
+    }
 };
--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,10 @@ const functions = require('./functions');
 test("Add 2+2 is equal to 4", () => {
     expect(functions.add(2,2)).toBe(4);
 });
+
+test("Should return Isfar Uddin", () => {
+    expect(functions.createUser()).toEqual({
+        firstName: "Isfar",
+        lastName: "Uddin"
+    });
+});
