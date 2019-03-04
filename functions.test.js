2f35e9fc757c89d17cf62442fe8457bc0abb80a6
isfar-uddin <isfarruett@gmail.com>
2019-03-04 16:43:53 +0600

    initialize database before and after test run

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,13 @@
 const functions = require('./functions');
 
+//beforeEach(()=> initDatabase());
+//afterEach(() => closeDatabase());
+beforeAll(() => initDatabase());
+afterAll(() => closeDatabase());
+
+const initDatabase = () => console.log("Initialize the database");
+const closeDatabase = () => console.log("Close the database");
+
 test("Add 2+2 is equal to 4", () => {
     expect(functions.add(2,2)).toBe(4);
 });
